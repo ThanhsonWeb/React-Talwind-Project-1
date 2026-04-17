85ba45fee8155487d40f51d6b27fb83499411262
ThanhsonWeb <sondeptroi@gmail.com>
2026-04-17 16:17:24 +0700

    style for Hero p-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,8 @@ import CourseItem from "./CourseItem";
 import des from "../assets/icon/des.svg";
 import dev from "../assets/icon/dev.svg";
 import marketing from "../assets/icon/marketing.svg";
+import Button from "./Button";
+import play from "../assets/icon/play.svg";
 
 function Hero() {
 	const colorDes = "bg-amber-100";
@@ -20,7 +22,7 @@ function Hero() {
 						className="h-[625px] w-[420px] rounded-2xl"
 					/>
 					{/* summarize */}
-					<div className="absolute right-0 top-[50%]  w-[270px] h-[244px] p-6 rounded-2xl bg-white ">
+					<div className="absolute right-0 top-[50%]  w-[270px] h-[244px] p-6 rounded-2xl bg-white  shadow-lg ">
 						<CourseItem color={colorDes} icon={des} title="20 courses">
 							UI/UX Design
 						</CourseItem>
@@ -34,7 +36,37 @@ function Hero() {
 				</div>
 
 				{/* Content */}
-				<div></div>
+				<div className="w-[50%] flex flex-col justify-center p-10 ">
+					<h1 className=" font-sen text-5xl font-semibold leading-[58px] mb-5 ">
+						Learn without limits and spread knowledge
+					</h1>
+					<p className=" primary-text leading-[30px]  mb-5 ">
+						Build new skills for that "this is my year" feeling with course
+						,certificates, and degrees from world-class universities and
+						companies.
+					</p>
+					{/* cta */}
+					<div className="flex gap-8 mb-8">
+						<Button>See Courses</Button>
+						<div className="flex items-center gap-7">
+							<div className="p-4 rounded-full bg-white  shadow-lg">
+								<img src={play} alt="play" />
+							</div>
+							<span className="font-semibold shadow-blue-700">Watch Video</span>
+						</div>
+					</div>
+
+					<p className="primary-text mb-3">Recent engagement</p>
+
+					<div className="flex space-x-5">
+						<p className="primary-text">
+							<strong className="text-4xl">50K</strong> Students
+						</p>
+						<p className="primary-text">
+							<strong className="text-4xl">70+</strong> Courses
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
